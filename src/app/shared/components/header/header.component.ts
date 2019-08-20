afa8f18206c0a9361046720c96c838f4dbb21fbf
Anton Budagov <a.budagov@artk.ai>
2019-08-20 14:01:04 +0300

    ~add: click outside search block;

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,21 +1,30 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, HostListener, OnInit, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.sass']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.sass']
 })
-export class HeaderComponent implements OnInit {
-  @Output() navToggle = new EventEmitter<boolean>();
-  search: boolean = false;
-  constructor() { }
+export class HeaderComponent {
+    @Output() navToggle = new EventEmitter<boolean>();
+    search: boolean;
 
-  ngOnInit() {
-  }
-  public navOpen() {
-    this.navToggle.emit(true);
-  }
-  public toggle() {
-    this.search = !this.search;
-  }
+    constructor(private elementRef: ElementRef) {
+        this.search = false;
+    }
+    @HostListener('document:click', ['$event.target'])
+    public onClick(target) {
+        const clickedInside = this.elementRef.nativeElement.contains(target);
+        if (!clickedInside) {
+            this.search = false;
+        }
+    }
+
+    public navOpen() {
+        this.navToggle.emit(true);
+    }
+
+    public toggle() {
+        this.search = !this.search;
+    }
 }
